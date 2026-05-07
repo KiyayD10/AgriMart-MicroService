324ce068b6714877feaeba31dd3e4f24cb29bc38
Robani Diansyah <melinting522@gmail.com>
2026-05-07 16:23:06 +0700

    feat(product-service): daftarkan prisma dan product module ke app module

--- api/services/product-service/src/app.module.ts
+++ api/services/product-service/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProductModule } from './product/product.module';
 
+// Root module product-service
 @Module({
   imports: [PrismaModule, ProductModule],
   controllers: [],
