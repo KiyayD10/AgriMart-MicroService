9b9871448451488070fd2ddb01ba3707e969b2d1
Robani Diansyah <melinting522@gmail.com>
2026-05-08 21:11:15 +0700

    feat(product): sesuaikan entity product dengan model prisma

--- api/services/product-service/src/product/entities/product.entity.ts
+++ api/services/product-service/src/product/entities/product.entity.ts
@@ -1,1 +1,11 @@
-export class Product {}
+// Model Product sesuai schema Prisma
+export class Product {
+  id: number;
+  nama: string;
+  deskripsi: string | null;
+  harga: number;
+  stok: number;
+  kategoriId: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
