22e43d7a71964a1cee3d37a38f5290b7b6e2106a
haidarRamadhan <haidar_ramadhan_@teknokrat.ac.id>
2026-06-08 13:53:19 +0700

    feat(login-pages): tambah login admin

--- cms/cms/src/pages/login.tsx
+++ cms/cms/src/pages/login.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useRouter } from "next/router";
-import { adminAccount } from "../data/auth";
+import { adminAccount } from "../data/users";
 
 /*
   --------------------
