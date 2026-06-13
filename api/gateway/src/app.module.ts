13669bfcdd6f02416d6475f25755686fbcaf9b1e
Robani Diansyah <melinting522@gmail.com>
2026-06-13 18:20:43 +0700

    feat(gateway): generate module controller service untuk semua proxy

--- api/gateway/src/app.module.ts
+++ api/gateway/src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { KategoriModule } from './kategori/kategori.module';
+import { ProductModule } from './product/product.module';
+import { AuthModule } from './auth/auth.module';
+import { OrderModule } from './order/order.module';
+import { InventoryModule } from './inventory/inventory.module';
 
 // Root module api-gateway
 @Module({
-  imports: [],
+  imports: [KategoriModule, ProductModule, AuthModule, OrderModule, InventoryModule],
   controllers: [],
   providers: [],
 })
