0ad266064271c4dfe1918abd518f701ede3f75a3
Robani Diansyah <melinting522@gmail.com>
2026-06-13 18:47:49 +0700

    feat(gateway): implementasi proxy service auth

--- api/gateway/src/auth/auth.service.ts
+++ api/gateway/src/auth/auth.service.ts
@@ -1,4 +1,24 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  private readonly baseUrl = process.env['AUTH_SERVICE_URL'];
+
+  constructor(private readonly httpService: HttpService) {}
+
+  async register(data: unknown): Promise<unknown> {
+    const response = await firstValueFrom(
+      this.httpService.post(`${this.baseUrl}/auth/register`, data),
+    );
+    return response.data;
+  }
+
+  async login(data: unknown): Promise<unknown> {
+    const response = await firstValueFrom(
+      this.httpService.post(`${this.baseUrl}/auth/login`, data),
+    );
+    return response.data;
+  }
+}
