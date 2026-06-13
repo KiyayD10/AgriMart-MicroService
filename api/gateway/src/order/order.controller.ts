b6f7d26c655851100cf9cbb425be2f5fed7c7a74
Robani Diansyah <melinting522@gmail.com>
2026-06-13 18:48:43 +0700

    feat(gateway): implementasi endpoint proxy order controller

--- api/gateway/src/order/order.controller.ts
+++ api/gateway/src/order/order.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { OrderService } from './order.service';
 
 @Controller('order')
-export class OrderController {}
+export class OrderController {
+  constructor(private readonly orderService: OrderService) {}
+
+  @Get()
+  async findAll(): Promise<unknown> {
+    return this.orderService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id', ParseIntPipe) id: number): Promise<unknown> {
+    return this.orderService.findOne(id);
+  }
+
+  @Post()
+  async create(@Body() data: unknown): Promise<unknown> {
+    return this.orderService.create(data);
+  }
+
+  @Patch(':id')
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() data: unknown,
+  ): Promise<unknown> {
+    return this.orderService.update(id, data);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id', ParseIntPipe) id: number): Promise<unknown> {
+    return this.orderService.remove(id);
+  }
+}
