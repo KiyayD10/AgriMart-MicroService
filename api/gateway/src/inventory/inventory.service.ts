e787c31e34ca8581a5b6b4b9273007504a1594ad
Robani Diansyah <melinting522@gmail.com>
2026-06-13 18:49:08 +0700

    feat(gateway): implementasi proxy service inventory

--- api/gateway/src/inventory/inventory.service.ts
+++ api/gateway/src/inventory/inventory.service.ts
@@ -1,4 +1,45 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
-export class InventoryService {}
+export class InventoryService {
+  private readonly baseUrl = process.env['INVENTORY_SERVICE_URL'];
+
+  constructor(private readonly httpService: HttpService) {}
+
+  async findAll(): Promise<unknown> {
+    const response = await firstValueFrom(
+      this.httpService.get(`${this.baseUrl}/inventory`),
+    );
+    return response.data;
+  }
+
+  async findOne(productId: number): Promise<unknown> {
+    const response = await firstValueFrom(
+      this.httpService.get(`${this.baseUrl}/inventory/${productId}`),
+    );
+    return response.data;
+  }
+
+  async create(data: unknown): Promise<unknown> {
+    const response = await firstValueFrom(
+      this.httpService.post(`${this.baseUrl}/inventory`, data),
+    );
+    return response.data;
+  }
+
+  async adjustStock(productId: number, data: unknown): Promise<unknown> {
+    const response = await firstValueFrom(
+      this.httpService.patch(`${this.baseUrl}/inventory/${productId}`, data),
+    );
+    return response.data;
+  }
+
+  async findLogs(productId: number): Promise<unknown> {
+    const response = await firstValueFrom(
+      this.httpService.get(`${this.baseUrl}/inventory/${productId}/logs`),
+    );
+    return response.data;
+  }
+}
