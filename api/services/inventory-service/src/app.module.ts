084ccd69d3a94af07e8d15b3672ac2f9ca4f4766
Robani Diansyah <melinting522@gmail.com>
2026-06-13 17:21:18 +0700

    feat(inventory): generate resource inventory

--- api/services/inventory-service/src/app.module.ts
+++ api/services/inventory-service/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
+import { InventoryModule } from './inventory/inventory.module';
 
 // Root module inventory-service
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, InventoryModule],
   controllers: [],
   providers: [],
 })
