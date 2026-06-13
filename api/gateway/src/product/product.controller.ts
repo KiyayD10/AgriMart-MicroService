dcad0c9b6711c80d375737b6b0d0da9443af92e3
Robani Diansyah <melinting522@gmail.com>
2026-06-13 18:47:23 +0700

    feat(gateway): implementasi endpoint proxy product controller

--- api/gateway/src/product/product.controller.ts
+++ api/gateway/src/product/product.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { ProductService } from './product.service';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+  constructor(private readonly productService: ProductService) {}
+
+  @Get()
+  async findAll(): Promise<unknown> {
+    return this.productService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id', ParseIntPipe) id: number): Promise<unknown> {
+    return this.productService.findOne(id);
+  }
+
+  @Post()
+  async create(@Body() data: unknown): Promise<unknown> {
+    return this.productService.create(data);
+  }
+
+  @Patch(':id')
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() data: unknown,
+  ): Promise<unknown> {
+    return this.productService.update(id, data);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id', ParseIntPipe) id: number): Promise<unknown> {
+    return this.productService.remove(id);
+  }
+}
