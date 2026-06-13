f236bfa7984ed527d855fdbdc9ee3f57b396f55e
Robani Diansyah <melinting522@gmail.com>
2026-06-13 18:41:14 +0700

    feat(gateway): implementasi proxy service product

--- api/gateway/src/product/product.service.ts
+++ api/gateway/src/product/product.service.ts
@@ -1,4 +1,45 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  private readonly baseUrl = process.env['PRODUCT_SERVICE_URL'];
+
+  constructor(private readonly httpService: HttpService) {}
+
+  async findAll(): Promise<unknown> {
+    const response = await firstValueFrom(
+      this.httpService.get(`${this.baseUrl}/product`),
+    );
+    return response.data;
+  }
+
+  async findOne(id: number): Promise<unknown> {
+    const response = await firstValueFrom(
+      this.httpService.get(`${this.baseUrl}/product/${id}`),
+    );
+    return response.data;
+  }
+
+  async create(data: unknown): Promise<unknown> {
+    const response = await firstValueFrom(
+      this.httpService.post(`${this.baseUrl}/product`, data),
+    );
+    return response.data;
+  }
+
+  async update(id: number, data: unknown): Promise<unknown> {
+    const response = await firstValueFrom(
+      this.httpService.patch(`${this.baseUrl}/product/${id}`, data),
+    );
+    return response.data;
+  }
+
+  async remove(id: number): Promise<unknown> {
+    const response = await firstValueFrom(
+      this.httpService.delete(`${this.baseUrl}/product/${id}`),
+    );
+    return response.data;
+  }
+}
