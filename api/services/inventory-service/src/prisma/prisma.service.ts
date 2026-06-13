e54995c033342ed8c2a0c64c53499e61986e1922
Robani Diansyah <melinting522@gmail.com>
2026-06-13 17:16:28 +0700

    feat(inventory-service): implementasi prisma service dengan adapter pg

--- api/services/inventory-service/src/prisma/prisma.service.ts
+++ api/services/inventory-service/src/prisma/prisma.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import 'dotenv/config';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '../generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
+// Service untuk jadi jembatan antara NestJS dan Prisma
 @Injectable()
-export class PrismaService {}
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    // Pakai adapter untuk koneksi prisma 7 ke PostgreSQL lokal
+    const adapter = new PrismaPg({
+      connectionString: process.env['DATABASE_URL'] as string,
+    });
+    super({ adapter });
+  }
+
+  // Konek ke DB pas module pertama kali jalan
+  async onModuleInit(): Promise<void> {
+    await this.$connect();
+  }
+
+  // Putus koneksi DB pas module di-destroy biar gak memory leak
+  async onModuleDestroy(): Promise<void> {
+    await this.$disconnect();
+  }
+}
