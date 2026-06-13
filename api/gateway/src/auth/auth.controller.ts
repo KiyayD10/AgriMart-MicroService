244fcca9ff2c446abc5e71d41af6d7f8c49868a2
Robani Diansyah <melinting522@gmail.com>
2026-06-13 18:48:01 +0700

    feat(gateway): implementasi endpoint proxy auth controller

--- api/gateway/src/auth/auth.controller.ts
+++ api/gateway/src/auth/auth.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  @HttpCode(HttpStatus.CREATED)
+  async register(@Body() data: unknown): Promise<unknown> {
+    return this.authService.register(data);
+  }
+
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  async login(@Body() data: unknown): Promise<unknown> {
+    return this.authService.login(data);
+  }
+}
